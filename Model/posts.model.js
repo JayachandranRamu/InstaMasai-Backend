b325e0b52b273558a392904c3f064872612ebb43
Jai <jayachandran9629040900@gmail.com>
2023-10-30 16:30:16 +0530

    Backend is done!

---
+++ Model/posts.model.js
@@ -0,0 +1,16 @@
+const mongoose=require("mongoose");
+
+const PostSchema=mongoose.Schema({
+    title:String,
+    body:String,
+    device:String,
+   no_of_comments:Number,
+   username:String,
+  user_id:String
+},{versionKey:false});
+
+const PostModel=mongoose.model("post",PostSchema);
+
+module.exports={
+    PostModel
+}
